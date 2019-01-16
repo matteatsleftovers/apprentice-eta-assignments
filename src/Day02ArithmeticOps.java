dd05d86979225b10bfc63d3b63dcc87495024a58
Matt Lauer <matteatsleftovers@gmail.com>
2019-01-16 09:41:53 -0500

    Solution to day 02 arithmetic ops problem

---
+++ src/Day02ArithmeticOps.java
@@ -0,0 +1,14 @@
+public class Day02ArithmeticOps {
+
+    public static void main(String[] args) {
+        double a = 26.2;
+        double b = 13.1;
+        System.out.println("a + b equals " + (a + b));
+        System.out.println("a - b equals " + (a - b));
+        System.out.println("a * b equals " + (a * b));
+        System.out.println("a / b equals " + (a / b));
+        System.out.println("The average of a and b is " + (a + b)/2);
+        System.out.println((a > b ? "a" : "b") + " is larger!");
+    }
+
+}
