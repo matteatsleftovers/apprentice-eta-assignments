ac42f02a1c00a3cbadf45d235a9cfe0adfb8d9e9
Matt Lauer <matteatsleftovers@gmail.com>
2019-01-16 16:14:20 -0500

    Solution to day 03 multiples problem

---
+++ src/Day03Multiples.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Day03Multiples {
+
+    public static void main(String[] args) {
+        int number;
+        do {
+            System.out.println("Please enter a number between 1 and 20:");
+            Scanner scanner = new Scanner(System.in);
+            number = scanner.nextInt();
+        } while (number < 1 || number > 20);
+
+        int multiple = 1;
+        while (multiple <= number) {
+            System.out.println("4 times " + multiple + " is " + 4 * multiple);
+            multiple++;
+        }
+    }
+
+}
