33185cf225687739d90b568e3d78d0100b7fa9a8
Matt Lauer <matteatsleftovers@gmail.com>
2019-01-16 10:31:24 -0500

    Solution to day 02 temperature converter problem

---
+++ src/Day02TemperatureConverter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day02TemperatureConverter {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter a temperature in Fahrenheit: ");
+        Scanner scanner = new Scanner(System.in);
+        String fahrenheitTempString = scanner.nextLine();
+        double fahrenheitTemp = Double.parseDouble(fahrenheitTempString);
+        double celsiusTemp = (fahrenheitTemp - 32) / 1.8;
+        System.out.println("All right, sweet. Your temperature in Fahrenheit is " + fahrenheitTemp + "°F");
+        System.out.println("Your temperature in Celsius is " + celsiusTemp + "°C");
+    }
+
+}
