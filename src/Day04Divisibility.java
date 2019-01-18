ff588f96d27538e28aae6596fa4a5c795397cc58
Matt Lauer <matteatsleftovers@gmail.com>
2019-01-18 09:01:20 -0500

    Brute force solution to day 04 divisibility problem

---
+++ src/Day04Divisibility.java
@@ -0,0 +1,17 @@
+public class Day04Divisibility {
+
+    public static void main(String[] args) {
+        int possibleNumber = 1;
+        int range = 20;
+        while (true) {
+            boolean sieve = true;
+            for (int i = 1; i <= range; i++) {
+                sieve &= (possibleNumber % i == 0);
+            }
+            if (sieve) break;
+            possibleNumber++;
+        }
+        System.out.println("That super divisible number is " + possibleNumber);
+    }
+
+}
