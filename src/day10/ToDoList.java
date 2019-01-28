8bb1b938c3441b5457973dcf5afc9627ff41819a
Matt Lauer <matteatsleftovers@gmail.com>
2019-01-28 11:19:55 -0500

    Add get & add methods & constructor for ToDoList

--- src/day10/ToDoList.java
+++ src/day10/ToDoList.java
@@ -1,4 +1,20 @@
 package day10;
 
+import java.util.ArrayList;
+
 public class ToDoList {
+
+    private ArrayList<ToDoListItem> items;
+
+    public ToDoList() {
+        items = new ArrayList<>();
+    }
+
+    public void addItem(String description) {
+        items.add(new ToDoListItem(description));
+    }
+
+    public ArrayList<ToDoListItem> getItems() {
+        return items;
+    }
 }
