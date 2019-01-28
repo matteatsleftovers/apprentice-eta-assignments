c75051690ade0f93a7a2ca7276cf441cda35d5f6
Matt Lauer <matteatsleftovers@gmail.com>
2019-01-28 10:56:45 -0500

    Add properties, getters, setters, and constructor for ToDoListItem

--- src/day10/ToDoListItem.java
+++ src/day10/ToDoListItem.java
@@ -1,4 +1,38 @@
 package day10;
 
 public class ToDoListItem {
+
+    private State state;
+    private String description;
+
+    public ToDoListItem(String description) {
+        this.state = State.TODO;
+        this.description = description;
+    }
+
+    public void startProgress() {
+        this.state = State.IN_PROGRESS;
+    }
+
+    public void markDone() {
+        this.state = State.DONE;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    enum State {TODO, IN_PROGRESS, DONE}
 }
